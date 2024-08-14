08b9ed94bfb2ef14cbbbe77d502c58472e9b4dae
Muchui William <williammuchui584@gmail.com>
2024-08-14 15:50:10 +0300

    url and request method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "Murl", about = "A rust based Curl", version = "0.1", author = "Muchui William", long_about = "None")]
+struct Args{
+    #[arg(short = 'u', long = "url")]
+    url: String,
+
+    #[arg(short = 'X')]
+    method: Option<String>,
+}
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let url = get_url()?;
+    println!("hello : {}", url);
+
+    let method = get_method()?;
+    println!("method: {}", method);
+    Ok(())
+}
+
+fn get_url()->Result<String, Box<dyn std::error::Error>>{
+    let args = Args::parse();
+
+    Ok(args.url)
+}
+
+fn get_method()-> Result<String, Box<dyn std::error::Error>>{
+    let args = Args::parse();
+    let method = match args.method {
+        Some(v) => v,
+        None => String::from("GET"),
+    };
+    Ok(method)
+    
 }
